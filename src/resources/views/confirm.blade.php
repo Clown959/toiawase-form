f12001a98865f538f5059a3198b9d1d53e8d27d2
Clown959 <koukithi3194@gold.megaegg.ne.jp>
2024-06-18 21:42:58 +0900

    message

---
+++ src/resources/views/confirm.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>FashionablyLate</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/confirm.css') }}" />
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo" href="/">
+        FashionablyLate
+      </a>
+    </div>
+  </header>
+
+  <main>
+    <div class="confirm__content">
+      <div class="confirm__heading">
+        <h2>Login</h2>
+      </div>
+      <form class="form" action="/contacts" method="post">
+      @csrf
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">メールアドレス</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="email" name="email" placeholder="test@example.com" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+        <div class="form__group">
+          <div class="form__group-title">
+            <span class="form__label--item">パスワード</span>
+          </div>
+          <div class="form__group-content">
+            <div class="form__input--text">
+              <input type="pass" name="pass" placeholder="coachtech1106" />
+            </div>
+            <div class="form__error">
+              <!--バリデーション機能を実装したら記述します。-->
+            </div>
+          </div>
+        </div>
+        <div class="form__button">
+          <button class="form__button-submit" type="submit">ログイン</button>
+        </div>
+      </form>
+    </div>
+  </main>
+</body>
+
+</html>
